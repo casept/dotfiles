a890d564e7e9d0b557ab6a2f540cbeb488d5bcfa
casept <davids.paskevics@gmail.com>
2025-02-14 16:59:52 +0100

    tmux: Start session logging from shell
    Seems like triggering it based on hooks is not reliable.

--- usr/bin/tmux_start_logging.sh
+++ usr/bin/tmux_start_logging.sh
@@ -5,7 +5,10 @@ logdir="$HOME/.cache/tmux"
 mkdir -p "$logdir"
 
 timestamp=$(date "+%F_%T")
-logfile="$logdir/tmux.$timestamp:#H.#S:#I:#W:#P.log"
+# Panes are started in quick succession by e.g. tmuxinator.
+# To prevent them logging into the same file, append a random slug to the name
+slug=$(tr -dc A-Za-z0-9 </dev/random | head -c 8; echo)
+logfile="$logdir/tmux.$timestamp:#H.#S:#I:#W:#P:$slug.log"
 
 # See https://nicokrieger.de/trick-tmux-into-auto-logging/
 tmux pipe-pane -o "cat >>$logfile"
