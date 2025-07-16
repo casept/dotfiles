83674459fda6b9557105ab641f4341f63c1847e4
casept <davids.paskevics@gmail.com>
2025-07-16 12:29:14 +0200

    kde: Set up the environment as for a login shell

---
+++ kde/.config/plasma-workspace/env/env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# KDE runs this script at startup and keeps any environment changes it makes.
+# Use this to make the env of the graphical session match a login shell.
+source ~/.profile
